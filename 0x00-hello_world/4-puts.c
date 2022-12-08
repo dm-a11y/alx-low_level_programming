1c626b82016f21c1da219da3c6b2ff6a3270f3dd
Daneil <daneilmehabaw55@gmail.com>
2022-12-08 11:25:12 -0800

    my first c code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main = main function
- *
+ * a program that prints a string.
  * Return: 0
  */
 int main(void)
 { 
-puts("\"Programming is like building a multilingual puzzle");
-return (0);      
+           puts("\"Programming is like building a multilingual puzzle");
+           return (0);      
 }
