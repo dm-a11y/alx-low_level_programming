3b78702eaecc35e9da038c834a84a28f405837d4
Daneil <daneilmehabaw55@gmail.com>
2022-12-12 02:10:53 -0800

    combinations of three digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * main prints all possible combinations of three digits in ascending order.
+ * Return: 0
+ */
+
+int main(void)
+{
+	int l, m, n;
+
+	for (l = 48; l <= 58; l++)
+	{
+		for (m = 49; m <= 58; m++)
+		{
+			for (n = 50; n <= 58; n++)
+			{
+				if (n > m && m > l)
+				{
+					putchar(l);
+					putchar(m);
+					putchar(n);
+					if (l != 55 || m != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
