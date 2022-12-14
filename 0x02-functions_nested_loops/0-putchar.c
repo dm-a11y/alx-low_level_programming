3d155dc21f67395df79c2dc42ba5e57480f1252e
Daneil <daneilmehabaw55@gmail.com>
2022-12-14 04:29:54 -0800

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - main prints _putchar followed by a new line.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n = 0;
+	char c[8] = "_putchar";
+	while (n < 0)
+	{
+		_putchar(c[n]);
+		n++;
+	}
+
+	_putchar('\n');
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,3 +5,5 @@
  * this header file all the function and prototypes that are used for this proij * ect.
  */
 int _putchar(char c);
+
+#endif
