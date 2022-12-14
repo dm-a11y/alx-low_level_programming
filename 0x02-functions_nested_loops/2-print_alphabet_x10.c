ae998b1d6e1e2734ec8fb060d2cadee0e59bed75
Daneil <daneilmehabaw55@gmail.com>
2022-12-14 12:25:40 -0800

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,9 +9,10 @@ void print_alphabet_x10(void)
 	char c;
 	for (n = 0; n <=9; n++)
 	{
-		for (c = "a"; c <= "z"; c++)
+		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
 			_putchar('\n');
 		}
 	}
+}
