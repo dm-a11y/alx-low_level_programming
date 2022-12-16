c525d689fa916273c4f9928039c44416c4e5df82
Daneil <daneilmehabaw55@gmail.com>
2022-12-16 08:34:57 -0800

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * isupper - prints whether the tha value passed to it is uppercase or not.
+ * _isupper - checks whether the value passed to it is uppercase or not.
+ * @c: Argument passed to the functio
  * Return: return 1 if success otherwise return 0
  */
 int _isupper(int c)
