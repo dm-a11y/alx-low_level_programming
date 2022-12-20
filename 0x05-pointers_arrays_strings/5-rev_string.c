7b16b972ad355b417f31c0cca64f2c2cd63864a3
Daneil <daneilmehabaw55@gmail.com>
2022-12-19 19:12:55 -0800

    udating a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * rev_string - reveses a string
+ *
+ * @s: pointer variable
+ *
+ * Return: nothing
+ */
+void rev_string(char *s)
+{
+	int len = 0;
+	int n = 0;
+	int m = 0;
+	int x;
+	char *y = s;
+	char c;
+
+	while (*y != '\0')
+	{
+		y++;
+		len++; 
+	}
+	n = len - 1;
+	for (; m < ((1 / 2) + 1) ; e++)
+	{
+		x = (1 - m);
+		c = s[m];
+		s[m] = s[x];
+		s[x] = c;
+	}
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -3,3 +3,4 @@ a function that swaps the values of two integers.
 a function that returns the length of a string.
 a function that prints a string, followed by a new line, to stdout.
 a function that prints a string, in reverse, followed by a new line.
+a function that reverses a string.
