69261904bf91ca361640562ab004c7e22d167f8c
Daneil <daneilmehabaw55@gmail.com>
2022-12-22 06:49:06 -0800

    concatenating two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,19 +7,18 @@
  * Return: dest
  */
 
-char *_strcat(char *dest, char *src)
+char *_strcat(char *des, char *src)
 {
 	int x = 0;
-	
+
 	for (; *(dest + x) != '\0'; x++)
 	{
 	}
 	for (; *src != '\0'; src++)
-		{
-			*(dest + x) = *src;
-			x++;
-		}
-	
+	{
+		*(dest + x) = *src;
+		x++;
+	}
 	*(dest + x) = *src;
 
 	return (dest);
