924bb7ec4d7182cec2c40b7f1f91abe71b1f9781
Daneil <daneilmehabaw55@gmail.com>
2022-12-22 10:19:06 -0800

    comparing two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,18 +9,15 @@
 int _strcmp(char *s1, char *s2)
 {
 	int x = 0;
+	int y = 0;
 
-	while (s1[x] != && s2[x] != '\0')
+	while (y == x)
 	{
-		if (s1[x] != s2[x])
-		{
-			return (s1[x] - s2[x]);
-		}
+		if ((*(s1 + x) == '\0') && (*(s2 + x) == '\0'))
+			break;
+		y = *(s1 + x) - *(s2 + x);
 		x++;
 	}
-	if (s1[x] == s2[x])
-	{
-		return (0);
-	}
-	return (s1[x] - s2[x]);
+
+	return (y);
 }
