4c7724ff83b92c4200149164a08962964a5efe66
Daneil <daneilmehabaw55@gmail.com>
2022-12-22 08:11:10 -0800

    copying a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,13 +9,14 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int x;
-	for (x = 0; x < n && src[c] != '\0'; x++)
+
+	for (x = 0; x < n && src[x] != '\0'; x++)
 	{
-		dest[c] = src[x];
+		dest[x] = src[x]:
 	}
 	for (; x < n; x++)
 	{
 		dest[x] = '\0';
 	}
-	return (dest); 
+	return (dest);
 }
