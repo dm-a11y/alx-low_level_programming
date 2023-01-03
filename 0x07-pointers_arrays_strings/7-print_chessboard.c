7e9d7671ef81cb6d61064c07e878c6eb0166d17a
Daneil <daneilmehabaw55@gmail.com>
2023-01-02 17:43:12 -0800

    chess is mental torture

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints chessboard
+ * @: pointer to a 2d array
+ *
+ * Return: Nothing
+ */
+void print_chessboard(char (*a)[8])
+{
+	int x, y;
+
+	for (x = 1; x <= 8; x++)
+	{
+		for (y = 0; y < 8; y++)
+			_putchar(a[0][y];
+				a++;
+	}
+	_putchar('\n');
+}
