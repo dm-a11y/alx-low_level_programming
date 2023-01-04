3d5853ce375cd2065509120cf59f3b8e64b80ed5
Daneil <daneilmehabaw55@gmail.com>
2023-01-04 13:12:21 -0800

    returnign a factorial of any given number

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * factorial - return the factorial of any given number
+ * @n: any given number
+ *
+ * Return: 0.
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (1);
+	else 
+		return (n * factorial(n  1));
+}
