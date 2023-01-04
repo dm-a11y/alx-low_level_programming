489f75d6202cfb6d0f27971caaa1f84339f8d2f6
Daneil <daneilmehabaw55@gmail.com>
2023-01-04 14:19:21 -0800

    using sqrt function in recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,28 +1,28 @@
 #include "main.h"
 
 /**
- * sqrt - returns the natural square root of a number
+ * sqrt1 - returns the natural square root of a number
  * @n: number
  * @m: iterator
  *
  * Return: 0.
  */
-int sqrt(int n, int m)
+int sqrt1(int n, int m)
 {
 	if (m * m == n)
 		return (m);
 	else if (m * m > n)
 		return (-1);
-	return (sqrt(n, b + 1));
+	return (sqrt1(n, m + 1));
 }
 
 /**
- * _sqrt_recursion - returns the natural square of a number
+ * _sqrt_recursion - returnsthe natural square root of a number
  * @n: number
  *
- * Return: 0.
+ * Return: 0
  */
 int _sqrt_recursion(int n)
 {
-	return (sqrt(n, 1));
+	return (sqrt1(n, 1));
 }
