69c31a19be545e780d24319849e349f3280b91f8
Daneil <daneilmehabaw55@gmail.com>
2023-01-08 07:08:26 -0800

    printing th product of two numbers using command line argument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the product of two numbers
+ * @argc: counts the number of arguments
+ * @argv: an array of pointers to the string
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
