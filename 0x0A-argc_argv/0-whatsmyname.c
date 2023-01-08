5165696f1052c48650ad37677cd2af34fe7eabec
Daneil <daneilmehabaw55@gmail.com>
2023-01-08 04:29:05 -0800

    printing the name of the program

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,10 +2,12 @@
 
 /**
  * main - prints the name of the program
+ * @argc: the numbers of arguments passed
+ * @argv: an array of pointers to the strings
  *
  * Return: Always 0.
  */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 
