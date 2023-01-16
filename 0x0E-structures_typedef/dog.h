b44cd643a2db0f264f88b9d454135ceaf7533c5a
Daneil <daneilmehabaw55@gmail.com>
2023-01-16 03:40:06 -0800

    poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef STDLIB_H
+#define STDLIB_H
+#include <stdlib.h>
+
+/**
+ * struct dog - short description
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
