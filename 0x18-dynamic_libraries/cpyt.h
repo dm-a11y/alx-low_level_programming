ffbbe20c9177360a8dc573210817a5cf6af98f1d
Daneil <daneilmehabaw55@gmail.com>
2023-03-24 13:09:08 -0700

    2nd dynamic library

---
+++ 0x18-dynamic_libraries/cpyt.h
@@ -0,0 +1,10 @@
+#ifndef CPYT_H
+#define CPYT_H
+
+int addnum(int x, int y);
+int subnum(int x, int y);
+int mulnum(int x, int y);
+float divnum(float x, float y);
+int modulnum(int x, int y);
+
+#endif
